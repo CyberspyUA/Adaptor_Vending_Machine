5e57211747a209d1bd736a609927829998e0826f
Nikita Struk <nikita.struk.softdev@gmail.com>
2023-05-11 21:19:18 +0300

    Project completed

--- Adaptor_Vending_Machine/Adaptor_Vending_Machine.cpp
+++ Adaptor_Vending_Machine/Adaptor_Vending_Machine.cpp
@@ -1,20 +1,124 @@
-// Adaptor_Vending_Machine.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+// Основний інтерфейс для торгового автомата
+class DrinkMachine {
+public:
+    virtual void chooseDrink() = 0;
+    virtual void addWater() = 0;
+    virtual void addSugar() = 0;
+    virtual void dispenseDrink() = 0;
+};
+// Адаптований інтерфейс для кавомашини
+class CoffeeMachine {
+public:
+    virtual void selectCoffee(const std::string& coffeeType) = 0;
+    virtual void addWater() = 0;
+    virtual void addSugar() = 0;
+    virtual void pourCoffee() = 0;
+};
+// Адаптер для кавомашини до інтерфейсу торгового автомата
+class CoffeeAdapter : public DrinkMachine {
+private:
+    CoffeeMachine* coffeeMachine_;
+public:
+    explicit CoffeeAdapter(CoffeeMachine* coffeeMachine) : coffeeMachine_(coffeeMachine) {}
 
-int main()
+    void chooseDrink() override {
+        coffeeMachine_->selectCoffee("звичайна");
+    }
+
+    void addWater() override {
+        coffeeMachine_->addWater();
+    }
+
+    void addSugar() override {
+        coffeeMachine_->addSugar();
+    }
+
+    void dispenseDrink() override {
+        coffeeMachine_->pourCoffee();
+    }
+
+};
+class MyCoffeeMachine : public CoffeeMachine {
+public:
+
+    void selectCoffee(const std::string& coffeeType) override {
+        std::cout << "Вибрана " << coffeeType << " кава.\n";
+    }
+
+    void addWater() override {
+        std::cout << "Заливаємо воду в кавоварку.\n";
+    }
+
+    void addSugar() override {
+        std::cout << "У кавоварку додаємо цукор.\n";
+    }
+
+    void pourCoffee() override {
+        std::cout << "Наливаю каву з кавомашини.\n";
+    }
+};
+class TeaMachine : public DrinkMachine  
 {
-    std::cout << "Hello World!\n";
+public:
+    void chooseDrink() override {
+        std::cout << "Обрано чай\n";
+    }
+
+    void addWater() override {
+        std::cout << "Долито до чаю воду.\n";
+    }
+
+    void addSugar() override {
+        std::cout << "Додано в чай цукор.\n";
+    }
+
+    void dispenseDrink() override {
+        std::cout << "Чай готовий!\n"
+            << "Смачного!\n";
+    }
+};
+class CacaoMachine : public DrinkMachine {
+public:
+    void chooseDrink() override {
+        std::cout << "Обрано какао\n";
+    }
+
+    void addWater() override {
+        std::cout << "Додано воду до какао.\n";
+    }
+
+    void addSugar() override {
+        std::cout << "До какао додано цукор.\n";
+    }
+
+    void dispenseDrink() override {
+        std::cout << "Какао готове!\n"
+            << "Смачного!\n";
+    }
+
+};
+// Код клієнта
+int main() {
+    setlocale(0, ".1251");
+    DrinkMachine* machine = new TeaMachine();
+    machine->chooseDrink();
+    machine->addWater();
+    machine->addSugar();
+    machine->dispenseDrink();
+
+    machine = new CoffeeAdapter(new MyCoffeeMachine());
+    machine->chooseDrink();
+    machine->addWater();
+    machine->addSugar();
+    machine->dispenseDrink();
+
+    machine = new CacaoMachine();
+    machine->chooseDrink();
+    machine->addWater();
+    machine->addSugar();
+    machine->dispenseDrink();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
